ae6a77680ff73c5cff059034ed669463b16de30f
nsouzarj <nsouzarj@outlook.com>
2022-12-16 11:56:41 -0300

    Atuliazacao do springsecurity

--- src/main/java/br/geral/SecurityConfig.kt
+++ src/main/java/br/geral/SecurityConfig.kt
@@ -6,70 +6,62 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.servlet.ViewResolver
 import org.springframework.web.servlet.view.InternalResourceViewResolver
 import javax.sql.DataSource
-import kotlin.jvm.Throws
 
 @Configuration
 @EnableWebSecurity(debug = false)
-@EnableGlobalMethodSecurity(prePostEnabled = true) // @ComponentScan(basePackages={"br.eti.nsouza.config","br.eti.nsouza.entidades","br.eti.nsouza.controle","br.eti.nsouza.repositorios","br.eti.nsouza.geral"})
-// @Configuration
 /**
  * Classe de segurança pra uso afins
  *
  * @author Nelson
  */
- open class SecurityConfig : WebSecurityConfigurerAdapter() {
-    var encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()!!
-    var encoder1 = BCryptPasswordEncoder()
+open class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     @Throws(Exception::class)
-    override  fun configure(http: HttpSecurity) {
 
-
-        // http.csrf().disable().authorizeRequests().antMatchers("/login","/resources/**",
-        // "/webjars/**","/erro/**").permitAll().anyRequest().authenticated().and().httpBasic();
-        println("Logou....maluco")
-        // http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-        //http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.GET,"/**").permitAll();
-
-        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.GET, "/webjars/**","/erro/**","/login","/uf/**", "/tipos/**", "/solicitacao/**", "/status/**", "/comarcapossui/**",
-                "/usuarios/**", "/colaborador/**", "/bancas/**", "/comarca/**", "/comarcas/**", "/**", "/").permitAll()
-
-        http.cors()
+    //  http.cors().disable();
+    override fun configure(http: HttpSecurity) {
+        http.csrf().disable().authorizeRequests().
+        antMatchers("/WEB-INF/views/","/webjars/**","/erro/**","/swagger-ui/**","/login/**","/js/**").permitAll().
+        antMatchers("/menu/**","/bancas/**","/comarcas/**","/comarca","/uf/**","/tipos/**","/comarcapossui/**","/solicitacoes/**","/usuarios/**").
+        hasAnyRole("USER","ADMIN").and().formLogin().loginPage("/login").loginProcessingUrl("/login").
+        successForwardUrl("/menu").failureForwardUrl("/").failureUrl("/");
+        //.and().authorizeRequests().antMatchers("/usuarios").hasAnyRole("ADMIN")
+        http.cors().disable();
     }
+
+
 
     @Autowired
     val data: DataSource? = null
 
     @Autowired
     @Throws(Exception::class)
-    override fun configure(builder: AuthenticationManagerBuilder) {
+    override fun configure(builder: AuthenticationManagerBuilder)  {
+
         println("Entrei....")
-        builder.jdbcAuthentication().dataSource(data).passwordEncoder(BCryptPasswordEncoder())
-                .usersByUsernameQuery(
-                        "select emailprincipal, senha as password, ativo from usuario where emailprincipal=?")
-                .authoritiesByUsernameQuery(
-                        "select emailprincipal, senha as password, ativo  from usuario where emailprincipal=?")
+//        builder.jdbcAuthentication().dataSource(data).passwordEncoder(BCryptPasswordEncoder())
+//                .usersByUsernameQuery(
+//                        "select emailprincipal, senha as password, ativo from usuario where emailprincipal=?")
+//                .authoritiesByUsernameQuery(
+//                        "select emailprincipal, senha as password, ativo  from usuario where emailprincipal=?")
 
-        // builder.inMemoryAuthentication()
-        // .withUser("tim").password("123").roles("ADMIN") .and()
-        // .withUser("nsouzarj@bol.com.br").password(encoder.encode("nso1810")).roles("USER");
+        builder.inMemoryAuthentication()
+            .withUser("admin").password(passwordEncoder()?.encode("admin")).roles("ADMIN").
+            and()
+            .withUser("user").password(passwordEncoder()?.encode("user")).roles("USER")
+
     }
 
-    @Bean
-     open fun  passwordEncoder(): PasswordEncoder {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
-    }
+
 
     @Bean
     open fun viewResolver(): ViewResolver {
@@ -79,11 +71,6 @@ import kotlin.jvm.Throws
         return viewResolver
     }
 
-    @Bean
-    @Throws(Exception::class)
-    override fun authenticationManagerBean(): AuthenticationManager {
-        return super.authenticationManagerBean()
-    }
 
     companion object {
         @JvmStatic
@@ -93,5 +80,10 @@ import kotlin.jvm.Throws
             val encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
             println(BCryptPasswordEncoder().encode("nso1810"))
         }
+    }
+
+    @Bean
+    open fun passwordEncoder(): BCryptPasswordEncoder? {
+        return BCryptPasswordEncoder()
     }
 }
