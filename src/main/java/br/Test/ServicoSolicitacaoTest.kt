93aa19b3a447f978d314962b5092d5e7d22c9333
nsouzarj <nsouzarj@outlook.com>
2023-01-27 20:11:56 -0300

    Alteracao no arquivo Docker para o latest mais recente

--- src/main/java/br/Test/ServicoSolicitacaoTest.kt
+++ src/main/java/br/Test/ServicoSolicitacaoTest.kt
@@ -1,15 +1,72 @@
 package br.Test
 
-import org.junit.Before
-import org.junit.Test
+import br.entidades.Solicitacao
+import br.servicos.ServicoSolicitacao
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Spy
 
-import org.junit.Assert.*
+
 
 class ServicoSolicitacaoTest {
 
-    @Before
+    @Spy
+    @InjectMocks
+    var servicoSolictacao: ServicoSolicitacao? = null
+
+    @Mock
+    var solicitacao: Solicitacao? =null
+
+    @BeforeEach
     fun setUp() {
+      println("Começo dos teste")
 
     }
+
+    @AfterEach
+    fun final(){
+        print("Final dos testes --")
+    }
+
+
+    @Test
+    @Order(2)
+    fun testeSave(){
+
+        solicitacao?.idsolicitacao =1
+        solicitacao?.audinterna=true
+        solicitacao?.convolada=true
+        servicoSolictacao?.save(solicitacao!!)
+
+    }
+
+    @Test
+    @Order(1)
+    fun testeDelete(){
+
+        solicitacao?.idsolicitacao =1
+        solicitacao?.audinterna=true
+        solicitacao?.convolada=true
+        servicoSolictacao?.delete(solicitacao?.idsolicitacao)
+
+    }
+
+    @Test
+    @Order(1)
+    fun testeFindSolictacao(){
+
+        solicitacao?.idsolicitacao =1
+        solicitacao?.audinterna=true
+        solicitacao?.convolada=true
+        var soli:Solicitacao= servicoSolictacao?.findById(solicitacao?.idsolicitacao)!!
+        Assertions.assertEquals(solicitacao,soli)
+    }
+
+
 
 }
